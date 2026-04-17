7e6d408d8d715dae220694a16c4680a6e238592f
JC - FullStack Dev <94090604+programandoconvalor@users.noreply.github.com>
2026-04-17 13:17:14 -0600

    Loom - V4

--- app/components/homepage/about/index.tsx
+++ app/components/homepage/about/index.tsx
@@ -72,7 +72,7 @@ export default function AboutSection() {
                     {areVideosPlaying ? (
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="h-7 w-7">
                         <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
-                      </svg>
+                      </svg> 
                     ) : (
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="h-8 w-8 translate-x-0.5">
                         <path d="M8 5v14l11-7z" />
